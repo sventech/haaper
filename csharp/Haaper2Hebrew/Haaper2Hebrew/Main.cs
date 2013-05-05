55a1e08e66623a6c5a1b3b06ec57345521acbca8
Sven Pedersen <sven.pedersen@gmail.com>
2013-05-05 18:46:29 -0500

    start processing files instead of fixed input, still hard coded
    reads input file and writes to output, hard-coded mode

--- csharp/Haaper2Hebrew/Haaper2Hebrew/Main.cs
+++ csharp/Haaper2Hebrew/Haaper2Hebrew/Main.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace Haaper2Hebrew
 {
@@ -8,6 +9,7 @@ namespace Haaper2Hebrew
 	{
 		public static void Main (string[] args)
 		{
+			string conversion = "tiqwah2unicode";
 			//string inputFile = args[0];
 			//string outputFile = args[1];
 			//StreamReader reader = new StreamReader(inputFile);
@@ -15,22 +17,47 @@ namespace Haaper2Hebrew
 			//reader.Close();
 			//StreamWriter writer = new StreamWriter(outputFile);
 
-			Console.OutputEncoding = Encoding.UTF8;
-			string input = "hatiqwah";
-			Console.WriteLine("input: "+input);
+			string input_name = @"/Users/svenpedersen/code/haaper/ten_commandments.tiq";
+			string output_name = @"/Users/svenpedersen/code/haaper/ten_commandments.txt";
 
-			Tiqwah2Unicode tiqwah2hebrew = new Tiqwah2Unicode();
-			Unicode2Tiqwah hebrew2tiqwah = new Unicode2Tiqwah();
-			Tiqwah2SAMPA tiqwah2phonetic = new Tiqwah2SAMPA();
+			var tiqwah2hebrew = new Tiqwah2Unicode();
+			var hebrew2tiqwah = new Unicode2Tiqwah();
+			var tiqwah2phonetic = new Tiqwah2SAMPA();
+			Xlator xlator;
+			if(conversion == "tiqwah2unicode") {
+				xlator = tiqwah2hebrew;
+			} else if(conversion == "unicode2tiqwah") {
+				xlator = hebrew2tiqwah;
+			} else if(conversion == "tiqwah2SAMPA") {
+				xlator = tiqwah2phonetic;
+			}
 
-			string output = tiqwah2hebrew.xlat(input);
-			Console.WriteLine("tiqwah2hebrew: "+output);
+			string line;
+			using (System.IO.StreamWriter outFile = new System.IO.StreamWriter(output_name))
+			{
+				// Read the file and process it line by line.
+				System.IO.StreamReader inFile = new System.IO.StreamReader(input_name);
+				while((line = inFile.ReadLine()) != null)
+				{
+					line = xlator.xlat(line);
+					outFile.WriteLine(line);
+				}
+				
+				inFile.Close();
+			}
 
-			string output2 = hebrew2tiqwah.xlat(output);
-			Console.WriteLine("hebrew2tiqwah: "+output2);
+			//Console.OutputEncoding = Encoding.UTF8;
+			//string input = "hatiqwah";
+			//Console.WriteLine("input: "+input);
 
-			string output3 = tiqwah2phonetic.xlat(input);
-			Console.WriteLine("tiqwah2SAMPA: "+output3);
+			//string output = tiqwah2hebrew.xlat(input);
+			//Console.WriteLine("tiqwah2hebrew: "+output);
+
+			//string output2 = hebrew2tiqwah.xlat(output);
+			//Console.WriteLine("hebrew2tiqwah: "+output2);
+
+			//string output3 = tiqwah2phonetic.xlat(input);
+			//Console.WriteLine("tiqwah2SAMPA: "+output3);
 		}
 	}
 }
